75126dd310bf40b078c852e2b0baaf6c9320e82a
gustavderdrache <aford@forumone.com>
2021-08-10 10:59:54 -0400

    Pin nixpkgs revision

--- pkgs.nix
+++ pkgs.nix
@@ -1,4 +1,7 @@
 # Entrypoint for derivations consuming the overlay (see default.nix).
-import <nixpkgs> {
+let
+  nixpkgs = builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/32fc8b9134c5fd56851ba1845f04d17484ea7170.tar.gz";
+in
+import nixpkgs {
   overlays = [(import ./overlay.nix)];
 }
